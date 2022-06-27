afe911156681668bcac72777bcf04c850e96955f
rubenaranda <94711120+rubenaranda@users.noreply.github.com>
2022-06-27 13:57:38 +0200

    Update search.js

--- search.js
+++ search.js
@@ -8,7 +8,3 @@ window.find(text);
 function searchBackward(text){
     return window.find(text,false,true,true,false);
 }
-
-nextButton.addEventListener("click",searchForward(document.getElementById('search').value));
-
-backButton.addEventListener("click",searchBackward(document.getElementById('search').value));
