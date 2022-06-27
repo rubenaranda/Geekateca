a1e2cac40c857ee2ae83b94b49726fae904a619a
rubenaranda <94711120+rubenaranda@users.noreply.github.com>
2022-06-27 13:28:44 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,8 @@
-function searchTitles() {
-    console.log('hola')
+let element = document.getElementById("search").value;
+let button = document.getElementById("submit");
+function searchForward(text){
+window.find(text);
+}
+function searchBackward(text){
+    return window.find(text,false,true,true,false);
 }
